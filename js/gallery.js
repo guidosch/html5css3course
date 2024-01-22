2659308231f7d7b3bb5798e18059f4284cf3822d
guidosch <guido.schnider@gmail.com>
2024-01-22 14:56:13 +0100

    jsHomeWork

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+
+function upDate(previewPic) {
+    let elem = document.getElementById('image')
+    elem.style.backgroundImage = "url('" + previewPic.src + "')";
+    elem.style.backgroundColor = "#red";
+    elem.innerHTML = previewPic.alt;
+}
+
+function unDo() {
+    let elem = document.getElementById('image')
+    elem.style.backgroundImage = "url('')";
+    elem.style.backgroundColor = "#8e68ff";
+    elem.innerHTML = "Hover over an image below to display here.";
+}
